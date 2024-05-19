ad33c9150ea71ae4a1236b48598aa1f06c0c3222
David-Jhon <106485025+David-Jhon@users.noreply.github.com>
2024-05-20 02:59:12 +0600

    Update textRoutes.js

--- routes/textRoutes.js
+++ routes/textRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Text = require('../models/text');
+const Text = require('./models/text');
 
 // Save text
 router.post('/save', async (req, res) => {
