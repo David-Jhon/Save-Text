8555f3134431ab9d8839c1764ba7474d9eb76172
David-Jhon <106485025+David-Jhon@users.noreply.github.com>
2024-05-20 02:35:08 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const textRoutes = require('./routes/textRoutes');
+
+const app = express();
+
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB', err));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api/texts', textRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
